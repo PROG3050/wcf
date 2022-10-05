0c63a545983c23072fb661bf7a979c6c903d10ba
Tapas Mandal <alloymobileinc@gmail.com>
2022-10-04 20:01:35 -0400

    Added login code

---
+++ Login/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.LoginClient loginClient = new ServiceReference1.LoginClient();
+            Console.WriteLine("Enter email:");
+            string user = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            string password = Console.ReadLine();
+            string res = loginClient.Login(user, password);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Login/LoginClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.LoginClient client = new ServiceReference1.LoginClient();
+            Console.WriteLine("Enter email:");
+            string user = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            string password = Console.ReadLine();
+            string res = client.Login(user, password);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
